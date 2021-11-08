b37366b53b91339663b71cfb3a0797ab73d67ed3
hyeoneedyou <hyeoneedyou@naver.com>
2021-11-08 23:47:21 +0900

    feat: 스프링 프로젝트 생성

---
+++ src/main/java/com/vidocs/vidocsserver/VidocsServerApplication.java
@@ -0,0 +1,13 @@
+package com.vidocs.vidocsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidocsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidocsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidocs/vidocsserver/VidocsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidocs.vidocsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidocsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
